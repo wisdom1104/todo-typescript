0a75ad535ab2813c080820c8c201952fcd0b4c27
wisdom1104 <jihye110530@gmail.com>
2023-06-22 22:30:52 +0900

    feat: todo 추가 기능 구현

---
+++ src/components/Form.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+interface NewTodo {
+  title: string;
+  content: string;
+  isDone: boolean;
+}
+const useAddComment = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: addComment } = useMutation({
+    mutationFn: async (newTodo: NewTodo) => {
+      await axios.post(`${process.env.REACT_APP_SERVER_URL}/todos`, newTodo);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["todos"]);
+    },
+  });
+
+  return { addComment };
+};
+
+function Form() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const { addComment } = useAddComment();
+  const onSubmitFormHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo = {
+      title,
+      content,
+      isDone: false,
+    };
+    addComment(newTodo);
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmitFormHandler}>
+        <div>
+          <span>제목: </span>
+          <input
+            maxLength={15}
+            type="text"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <span> 내용: </span>
+          <input
+            maxLength={50}
+            type="text"
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" value="추가하기">
+          추가하기
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Form;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Form from "../components/Form";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <Form />
+    </>
+  );
 }
 
 export default Home;
