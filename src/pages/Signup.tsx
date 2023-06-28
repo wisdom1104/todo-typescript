e48dc6a2381a68af1d731c7c264ed000daaf0b34
wisdom1104 <jihye110530@gmail.com>
2023-06-29 00:05:13 +0900

    feat: 회원가입 기초 기능 구현

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,34 +1,112 @@
-import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+interface NewUser {
+  email: string;
+  password: string;
+  nickname: string;
+}
+const useAddUser = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: addUser } = useMutation({
+    mutationFn: async (newUser: NewUser) => {
+      await axios.post(`${process.env.REACT_APP_SERVER_URL}/user`, newUser);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["user"]);
+      alert("회원가입 성공");
+    },
+  });
+
+  return { addUser };
+};
+
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
+  const { addUser } = useAddUser();
+  const onSubmitFormHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newNser = {
+      email,
+      password,
+      nickname,
+    };
+    addUser(newNser);
+    setEmail("");
+    setPassword("");
+    setPasswordCheck("");
+    setNickname("");
+  };
+
+  const [passwordCheckPwMsg, setpasswordCheckPwMsg] = useState("");
+  const onChangePasswordCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const checkPw = e.target.value;
+
+    if (password.length >= 1 && password !== checkPw) {
+      setpasswordCheckPwMsg("비밀번호가 일치하지 않습니다.");
+    }
+    if (password.length >= 1 && password === checkPw) {
+      setpasswordCheckPwMsg("비밀번호가 일치합니다.");
+    }
+  };
   return (
-    <form>
+    <form onSubmit={onSubmitFormHandler}>
       <div>Signup</div>
       <div>
         <span>이메일</span>
-        <input />
+        <input
+          type="email"
+          value={email}
+          placeholder="이메일을 입력해 주세요."
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
       <div>
         <span>닉네임</span>
-        <input />
+        <input
+          type="text"
+          value={nickname}
+          placeholder="닉네임을 입력해 주세요."
+          onChange={(e) => {
+            setNickname(e.target.value);
+          }}
+        />
       </div>
       <div>
         <span>비밀번호</span>
-        <input />
+        <input
+          type="password"
+          value={password}
+          placeholder="비밀번호를 입력해 주세요."
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
       <div>
         <span>비밀번호 확인</span>
-        <input />
+        <input
+          type="password"
+          value={passwordCheck}
+          placeholder="비밀번호를 다시 입력해 주세요."
+          onChange={(e) => {
+            setPasswordCheck(e.target.value);
+            onChangePasswordCheck(e);
+          }}
+        />
+        <div>{passwordCheckPwMsg}</div>
       </div>
-      <button
-        type="submit"
-        onClick={() => {
-          alert("회원가입 성공");
-        }}
-      >
-        회원가입
-      </button>
+      <button type="submit">회원가입</button>
       <Link to="/login">로그인</Link>
     </form>
   );
